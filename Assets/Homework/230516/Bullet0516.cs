80d68f160d3f5fa3925d07029c5920937935485e
Hyeonuk Ryu <rpoiu053@gmail.com>
2023-05-17 13:03:58 +0900

    feat : 0516
    1,2,3,번

---
+++ Assets/Homework/230516/Bullet0516.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]                       // rigidbody를 반드시 가지도록 해줌
+
+public class Bullet0516 : MonoBehaviour
+{
+    [SerializeField] private float bulletSpped;             // bullet의 움직일 속도
+    [SerializeField] private GameObject explosionEffect;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();                     // 해당 오브젝트의 rigidbody를 지정 
+    }
+
+    private void Start()
+    {
+        rb.velocity = transform.forward * bulletSpped;      // 가속도 없이 이동
+        Destroy(gameObject, 5f);                            // 생성된 후 5초 후면 사라짐
+    }
+
+    private void OnCollisionEnter(Collision collision)      // bullet의 충돌을 체크
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);                                // bullet 삭제
+    }
+}
---
+++ Assets/Homework/230516/MoveTank0516.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class MoveTank0516 : MonoBehaviour
+{
+    GameObject gameObject;
+
+    private Vector3 moveDir;        // 움직일 좌표
+
+    private int movePower = 5;      // 움직일 힘
+    private int rotatePower = 50;   // 회전할 힘
+
+    void Update()                   // 매 프레임마다 각도와 위치 계산
+    {
+        Move();
+        Rotation();
+    }
+
+    private void Move()             // transform을 이용하여 다음 좌표로 순간이동시키는 개념
+    {
+        transform.Translate(Vector3.forward * moveDir.z * movePower * Time.deltaTime, Space.Self);
+    }   //                                                              시간값,        해당 오브젝트 기준
+
+    private void Rotation()         // Rotate로 회전할(바라볼) 각을 지정
+    {
+        transform.Rotate(Vector3.up * moveDir.x * rotatePower * Time.deltaTime);
+        // transform.rotation = Quaternion.Euler(0, moveDir.x , moveDir.y);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveDir.x = value.Get<Vector2>().x;
+        moveDir.z = value.Get<Vector2>().y;
+    }
+}
